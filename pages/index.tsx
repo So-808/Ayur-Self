e01e9d5484c04c570505c2f12ec4012170d31402
So-808 <makesence808@gmail.com>
2025-07-15 17:24:59 +0900

    Create index.tsx
    初期ページ

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+// pages/index.tsx
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>🌿 AYUR SELF</h1>
+      <p>ようこそ。アダプトゲンハーブの世界へ。</p>
+      <p>このページはVercel上に正常に表示されています。</p>
+    </div>
+  );
+}
